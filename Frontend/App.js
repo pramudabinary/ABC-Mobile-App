3fafbf4a771bd10cca178e39e9062e556d97e2b1
Pramuda Liyanage <pramudatharika@gmail.com>
2021-11-09 19:39:23 +0530

    Signup and Login Complete

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,3 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+// import 'react-native-gesture-handler';
+import LoginComponent from './components/LoginComponent';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import SignUpComponent from './components/SignUpComponent';
+import DashBoardComponent from './components/DashBoardComponent';
 
-const App = ()
+const Stack = createStackNavigator();
+
+export default class App extends Component   {
+  render() {
+    return (
+      <NavigationContainer>
+      <Stack.Navigator screenOptions={{header: () => null}}>
+        <Stack.Screen name="SignIn" component={LoginComponent} />
+        <Stack.Screen name="SignUp" component={SignUpComponent} />
+        <Stack.Screen name="Home" component={DashBoardComponent} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    )
+  }
+}
